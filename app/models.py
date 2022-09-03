fa38d26bf70fcf862ea1bcc856d0b64ead0fbbac
Sahil <sahilsingh7977@gmail.com>
2022-09-03 16:06:39 +0530

    added table to display data

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class task(models.Model):
+    Name=models.CharField(max_length=20)
+    task=models.CharField(max_length=1000)
+    review=models.BooleanField(default=False)
--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
-from unicodedata import name
 from django.shortcuts import render,redirect
+from .models import task
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def user (request,name):
-    return render(request,'user.html',{'user':name})
+    list=task.objects.filter(Name=name,review=False).values()
+    return render(request,'user.html',{'user':name,'list':list,'i':0,'len':list.count})
 
 def user1(request):
     name=request.POST['user']
