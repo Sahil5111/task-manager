7306645ecf5468b318a70e2277c6929a7e7808d8
Sahil <sahilsingh7977@gmail.com>
2022-09-03 14:50:04 +0530

    added code to redirect user to pending stuffs

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('user/<str:name>',views.user,name='user'),
+    path('user',views.user1,name='user1'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from unicodedata import name
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def user (request,name):
+    return render(request,'user.html',{'user':name})
+
+def user1(request):
+    name=request.POST['user']
+    return redirect('user/'+name)
